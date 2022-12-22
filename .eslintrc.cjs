2182c18c57f96c1039b4f797dd20a4600cd1ac4e
Bence Márkus <markus.bence@icloud.com>
2022-12-22 16:28:13 +0100

    prettier config added to eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,22 @@
 /* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution');
 
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
-  extends: ['eslint:recommended', 'plugin:vue/vue3-essential', '@vue/eslint-config-typescript'],
+  env: {
+    'shared-node-browser': true,
+  },
+  extends: [
+    'prettier',
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    '@vue/eslint-config-typescript',
+  ],
   parserOptions: {
     ecmaVersion: 'latest',
   },
+  ignorePatterns: ['node_modules', 'dist'],
   rules: {
     'no-undef': 'off',
   },
