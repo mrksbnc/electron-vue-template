7c2c1843117457d7e0ae2335ab97a162f47d548c
Bence Márkus <markus.bence@icloud.com>
2022-12-22 16:18:28 +0100

    minor vue changes

--- packages/renderer/src/router.ts
+++ packages/renderer/src/router.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 
-const routes = [{ path: '/', component: () => import('./components/HelloWorld.vue') }];
+const routes = [{ path: '/', component: () => import('./views/DashboardView.vue') }];
 
 const router = createRouter({
   history: createWebHashHistory(),
---
+++ packages/renderer/src/views/DashboardView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex h-full items-center justify-center">
+    <span class="font-bold text-gray-600"> Hello world! </span>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
