c2fcab79fa8b2b59471a85f6e4a1226485d1762c
Bence Markus <markus.bence@icloud.com>
2023-03-30 09:55:55 +0200

    dependency bump

--- packages/renderer/tailwind.config.cjs
+++ packages/renderer/tailwind.config.cjs
@@ -11,7 +11,18 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#3B82F6',
+        'secondary': '#6B7280',
+        'success': '#10B981',
+        'danger': '#EF4444',
+        'warning': '#F59E0B',
+        'info': '#3B82F6',
+        'light': '#F3F4F6',
+        'dark': '#1F2937',
+      },
+    },
   },
   plugins: [],
 };
